f2a90a7ad2f9b9d0b255dbac2f3d99e8096dead9
vividron <ronaks21205@gmail.com>
2026-03-16 19:22:11 +0530

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className='text-lg font-extrabold text-black h-screen w-screen flex justify-center items-center'>
+      Test
+    </div>
+  )
+}
+
+export default App
