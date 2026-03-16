b871d9c3cdb5f5b0409161d8aa6aada6d1f51970
vividron <ronaks21205@gmail.com>
2026-03-16 19:51:52 +0530

    feat: Add api paths

---
+++ src/api/utils/apiPaths.js
@@ -0,0 +1,32 @@
+const API_PATHS = {
+    ROOM: {
+        CREATE: "/rooms/create",
+        GET_ALL: "/rooms",
+        GET_STATE: (roomId) => `/rooms/${roomId}`,
+        JOIN: (roomId) => `/rooms/${roomId}/join`,
+        LEAVE: (roomId) => `/rooms/${roomId}/leave`,
+        DELETE: (roomId) => `/rooms/${roomId}`,
+    },
+
+    SPOTIFY: {
+        GET_PLAYLISTS: "/spotify/playlists",
+        GET_PLAYLIST_ITEMS: (playlistId) => `/spotify/playlists/${playlistId}/items`,
+    },
+
+    PLAYER: {
+        STATE: (roomId) => `/rooms/${roomId}/player/state`,
+        RESUME: (roomId) => `/rooms/${roomId}/player/resume`,
+        PAUSE: (roomId) => `/rooms/${roomId}/player/pause`,
+        NEXT: (roomId) => `/rooms/${roomId}/player/next`,
+        PREVIOUS: (roomId) => `/rooms/${roomId}/player/previous`,
+        SEEK: (roomId) => `/rooms/${roomId}/player/seek`,
+    },
+
+    QUEUE: {
+        UPVOTE_MATCHES: (roomId) => `/rooms/${roomId}/queue/songs/upvote-matches`,
+        ADD_PLAYLIST: (roomId) => `/rooms/${roomId}/queue/playlist`,
+        CLEAR: (roomId) => `/rooms/${roomId}/queue`,
+    },
+};
+
+export default API_PATHS;
