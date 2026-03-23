a76da06a296446f1187400afb96022f54e77c5c2
vividron <ronaks21205@gmail.com>
2026-03-23 15:37:35 +0530

    feat(toast): Add toaster

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
+      <Toaster position='top-right' toastOptions={{ duration: 3000, style: {backgroundColor: "var(--bg-surface)", color: "var(--text-primary)", border: "2px solid grey"} }} />
       <App />
     </AuthProvider>
   </StrictMode>,
