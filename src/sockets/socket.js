73257c6a8ff622f8ba807a5078d80062bb009e00
vividron <ronaks21205@gmail.com>
2026-04-04 00:55:24 +0530

    feat(socket): add socket client wrapper.

---
+++ src/sockets/socket.js
@@ -0,0 +1,59 @@
+import { io } from "socket.io-client";
+
+const URL = import.meta.env.VITE_WS_URL;
+
+const socket = io(URL, {
+    withCredentials: true,
+    transports: ["websocket"],
+    autoConnect: false,
+});
+
+export const connectsocket = () => {
+    const token = localStorage.getItem("authToken");
+    if (!token) throw new Error("Token not found. Please sign in again.");
+    socket.auth = { token };
+    socket.connect();
+}
+
+export const disconnectSocket = () => {
+    socket.disconnect();
+}
+
+export const isConnected = () => {
+    return socket.connected;
+}
+
+export const listenEvent = (event, callback) => {
+    socket.on(event, callback);
+};
+
+export const removeListener = (event, callback) => {
+    socket.off(event, callback);
+};
+
+export const emitEvent = (event, data = {}, ack) => {
+    socket.emit(event, data, ack);
+};
+
+export const emitWithRetry = (event, data = {}, retries = 5, timeout = 1000) => {
+    return new Promise((resolve, reject) => {
+        let attempts = 0;
+
+        const tryEmit = () => {
+            attempts++;
+
+            socket.timeout(timeout).emit(event, data, (err, response) => {
+                if (err) {
+
+                    if (attempts >= retries) {
+                        return reject(new Error("Server not responding"));
+                    }
+                    // retry
+                    return tryEmit();
+                }
+                resolve(response);
+            });
+        };
+        tryEmit();
+    });
+};
