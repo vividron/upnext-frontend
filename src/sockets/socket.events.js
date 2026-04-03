d39234b098db888276d5422ee8a7d30088787cc2
vividron <ronaks21205@gmail.com>
2026-04-04 00:52:14 +0530

    feat(socket): Add socket events

---
+++ src/sockets/socket.events.js
@@ -0,0 +1,16 @@
+export const EVENTS = {
+    ROOM_SUBSCRIBE: "room:subscribe",
+    ROOM_MEMBER_COUNT: "room:member-count",
+    ROOM_END: "room:end",
+
+    PLAYER_STATE: "player:state",
+    PLAYER_PAUSE: "player:pause",
+    PLAYER_RESUME: "player:resume",
+    PLAYER_NEXT: "player:next",
+    PLAYER_SEEK: "player:seek",
+
+    QUEUE_ADD_SONGS: "queue:add-songs",
+    QUEUE_CLEAR: "queue:clear",
+    QUEUE_VOTE_SONG: "queue:vote-song",
+    QUEUE_SCORES_UPDATED: "queue:scores-updated"
+};
