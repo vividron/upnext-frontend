0aeb3c5048152771c32716c51f4c83a6ac5aaf82
vividron <ronaks21205@gmail.com>
2026-04-11 18:57:22 +0530

    feat: Add page not found page.

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { Ghost } from "lucide-react";
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
-  )
-}
+    <div className="h-screen flex flex-col gap-3 items-center justify-center text-main">
+      <Ghost size={60} />
 
-export default PageNotFound
+      <h1 className="text-6xl font-bold">404</h1>
+
+      <p className="text-lg">Oops! Page not found</p>
+
+      <p className="text-sm text-sub mt-1">
+        Looks like this page took a day off.
+        Even Google gave up searching.
+      </p>
+
+      <Link
+        to="/#hero"
+        className="mt-2 px-4 py-2 rounded-md bg-surface-container text-sm hover:bg-(--surface-container-high) transition"
+      >
+        Home
+      </Link>
+    </div>
+  );
+};
+
+export default PageNotFound;
