b8f39470f0faa53a0e486fc714b083a4253cd258
vividron <ronaks21205@gmail.com>
2026-04-11 15:25:32 +0530

    refactor(api): increase api request timeout.

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
-    timeout: 10000,
+    timeout: 30000,
     headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
